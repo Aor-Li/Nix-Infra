fd10d9f640946047610cba50064249d56138e0f4
l00809570 <liyifeng17@hisilicon.com>
2025-09-29 20:36:25 +0800

    temporal

--- modules/features/nix/secret.nix
+++ modules/features/nix/secret.nix
@@ -1,17 +1,32 @@
 { inputs, ... }:
 let
-  name = "private/nix/secret";
+  name = "feature/nix/secret";
 in
 {
   flake.modules.homeManager.${name} =
-    { config, pkgs, ... }:
+    {
+      config,
+      pkgs,
+      userConfig,
+      ...
+    }:
     {
       imports = [
         inputs.sops-nix.homeManagerModules.sops
       ];
+
       home.packages = [
         pkgs.age
         pkgs.sops
       ];
+
+      sops = {
+        age.keyFile = "/home/${userConfig.username}/.config/sops/age/keys.txt";
+        defaultSopsFile = ../../../secrets/secrets.yaml;
+      };
+
+      #programs.bash.sessionVariables = {
+      #  TEST_SECRET = config.sops.secrets.github_access_token;
+      #};
     };
 }
