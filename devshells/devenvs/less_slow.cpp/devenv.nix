730e7970d392ab2f98dbdbb44e6a0ba9ab548d0f
aor <liyifeng0039@gmail.com>
2025-10-22 03:11:36 +0800

    add less_slow.cpp

---
+++ devshells/devenvs/less_slow.cpp/devenv.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  enterShell = ''
+    clear
+    fastfetch
+  '';
+
+  packages = [
+    pkgs.clang-tools
+    pkgs.gcc15
+
+    pkgs.cmake
+    pkgs.ninja
+    pkgs.gnumake
+
+    pkgs.cudaPackages.cuda_cudart
+    pkgs.cudaPackages.cudatoolkit
+
+    pkgs.liburing
+  ];
+
+  env = {
+    CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
+  };
+
+  scripts = {
+    cm.exec = "cmake -S ./less_slow.cpp -B ./build -DCMAKE_EXPORT_COMPILE_COMMANDS=On";
+  };
+}
