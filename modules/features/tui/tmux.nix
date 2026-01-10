5f63cced19b46d3bdda961f9e27a7344d05fcbdd
aor <liyifeng0039@gmail.com>
2026-01-11 01:07:52 +0800

    updating tmux config

--- modules/features/tui/tmux.nix
+++ modules/features/tui/tmux.nix
@@ -13,8 +13,10 @@ in
           # basic settings
           clock24 = true;
           baseIndex = 1;
+
           # Stop tmux+escape craziness.
           escapeTime = 0;
+          
           # Force tmux to use /tmp for sockets (WSL2 compat)
           secureSocket = false;
 
@@ -27,45 +29,49 @@ in
 
           # configuration file
           extraConfig = ''
-            # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+            ###################################################################
+            ###  Basic Function Settings                                    ###
+            ###################################################################
+            set-option -g mouse on
+
+            # do not rename window automaticly
+            set-option -g allow-rename off
+            
+            # renumber when window closed
+            set -g renumber-window on
+            set -sg escape-time 100
+            
+            ###################################################################
+            ###  Basic KeyBinds Settings                                    ###
+            ###################################################################
+            bind r source-file ~/.config/tmux/tmux.conf # reload config
+            bind c new-window -c "#{pane_current_path}"
+            bind | split-window -h -c "#{pane_current_path}"
+            bind - split-window -v -c "#{pane_current_path}"
+            bind '"' split-window -v -c "#{pane_current_path}"
+            bind % split-window -h -c "#{pane_current_path}"
+           
+            ###################################################################
+            ###  Appearance Settings                                        ###
+            ###################################################################
+
+            # --- colors --- 
+            # (https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/)
             set -g default-terminal "xterm-256color"
             set -ga terminal-overrides ",*256col*:Tc"
             set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
             set-environment -g COLORTERM "truecolor"
 
-            # reload config file (change file location to your the tmux.conf you want to use)
-            bind r source-file ~/.config/tmux/tmux.conf
-
-            # mouse works as expected
-            set-option -g mouse on
-
-            # easy-to-remember split pane commands
-            bind | split-window -h -c "#{pane_current_path}"
-            bind - split-window -v -c "#{pane_current_path}"
-            bind c new-window -c "#{pane_current_path}"
-
-            # start new pane at current path
-            bind '"' split-window -v -c "#{pane_current_path}"
-            bind % split-window -h -c "#{pane_current_path}"
-
-            # do not rename window automaticly
-            set-option -g allow-rename off
-
-            # put status bar at top
-            set-option -g status-position top
-
-            # renumber when window closed
-            set -g renumber-window on
-
-            set -sg escape-time 100
-
-            # plugins: catppuccin
+            # --- themes: catppuccin ---
             set -g @catppuccin_flavor "mocha"
             set -g @catppuccin_window_status_style "slanted" # idk why but rounded not work for me
-            # show window name in tabs
             set -g @catppuccin_window_default_text "#W"
             set -g @catppuccin_window_current_text "#W"
             set -g @catppuccin_window_text "#W"
+            
+            # --- status bar ---
+            set-option -g status-position top
+
             run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
             set -g status-right-length 100
             set -g status-left-length 100
