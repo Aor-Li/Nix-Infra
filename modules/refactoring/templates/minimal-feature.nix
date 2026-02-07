f0ff41aaca25bd18364dcb969e3ab0f5985f8178
aor <liyifeng0039@gmail.com>
2026-02-08 06:51:14 +0800

    add template

---
+++ modules/refactoring/templates/minimal-feature.nix
@@ -0,0 +1,43 @@
+# 这是一个最简单的功能模板
+{ self, lib, ... }:
+let
+  inherit (self.meta) namespace;
+  path = [
+    feature
+    template
+    foo
+  ];
+in
+{
+  flake.modules = {
+    nixos = lib.setAttrByPath path (
+      {
+        config,
+        lib,
+        hostConfig,
+        ...
+      }:
+      {
+        options.namespace = lib.setAttrByPath path {
+          enable = lib.mkEnableOption "Enable the template";
+        };
+        config = { };
+      }
+    );
+
+    homeManager = lib.setAttrByPath path (
+      {
+        config,
+        lib,
+        userConfig,
+        ...
+      }:
+      {
+        options.namespace = lib.setAttrByPath path {
+          enable = lib.mkEnableOption "Enable the template";
+        };
+        config = { };
+      }
+    );
+  };
+}
