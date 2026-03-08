363dbb8309a3ce28dbaefccee5985a5a1ec12142
aor <liyifeng0039@gmail.com>
2026-03-09 02:49:20 +0800

    update niri and dms

--- modules/features/desktop/session/niri.nix
+++ modules/features/desktop/session/niri.nix
@@ -1,9 +1,16 @@
+{ inputs, ... }:
 {
   flake.aor.modules.feature.desktop.session.niri = {
     nixos =
       { ... }:
       {
         programs.niri.enable = true;
+      };
+
+    home =
+      { ... }:
+      {
+        imports = [ inputs.niri.homeModules.niri ];
       };
   };
 }
--- modules/features/desktop/shell/quickshell/presets/dank-material-shell.nix
+++ modules/features/desktop/shell/quickshell/presets/dank-material-shell.nix
@@ -9,9 +9,14 @@ in
       {
         imports = [
           inputs.dms.homeModules.dank-material-shell
+          inputs.dms.homeModules.niri
         ];
 
-        programs.dank-material-shell.enable = true;
+        programs.dank-material-shell = {
+          enable = true;
+          niri.enableSpawn = true; # Auto-start DMS with niri, if enabled
+          systemd.enable = false;
+        };
       };
   };
 }
