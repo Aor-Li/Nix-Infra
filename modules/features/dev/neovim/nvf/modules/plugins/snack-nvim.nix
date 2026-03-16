0f13fb20244deeda1db68625cc4e70cb499e2755
l00809570 <liyifeng17@hisilicon.com>
2026-03-16 20:49:50 +0800

    udpate snacks

--- modules/features/dev/neovim/nvf/modules/plugins/snack-nvim.nix
+++ modules/features/dev/neovim/nvf/modules/plugins/snack-nvim.nix
@@ -1,7 +1,13 @@
 {
   flake.aor.modules.feature.dev.neovim.nvf.home = {
-    programs.nvf.settings.vim = { 
-      
+    programs.nvf.settings.vim = {
+      utility.snacks-nvim = {
+        enable = true;
+        setupOpts = {
+          dashboard.enable = false;
+          explorer.enable = false;
+        };
+      };
     };
   };
 }
